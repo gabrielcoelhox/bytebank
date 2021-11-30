cb5864d4c529e24e5486a6db8305acc9f71e0e82
Gabriel Coelho <gabriel.coelhox@gmail.com>
2021-11-29 22:41:44 -0300

    Exportando dados do componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'bytebank';
+
+  transferencia: any = {};
+
+  transferir($event) {
+    console.log($event);
+    this.transferencia = $event;
+  }
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,6 @@
+import { Output } from '@angular/core';
 import { Component } from '@angular/core';
+import { EventEmitter } from 'stream';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -7,12 +9,20 @@ import { Component } from '@angular/core';
 })
 export class NovaTransferenciaComponent {
 
-  valor!: number;
-  destino!: number;
+  @Output() aoTransferir = new EventEmitter();
+
+  valor: number;
+  destino: number;
 
   transferir() {
     console.log('Solicitada nova transferência');
-    console.log('Valor: ', this.valor);
-    console.log('Destino: ', this.destino);
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
   }
 }
